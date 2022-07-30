0a3de525612fd73831dad5ec9d1870e4685f2ee2
Lambert <595265454@qq.com>
2022-07-30 16:08:58 +0800

    全部学术硕士信息方法维护

--- main_mysql.py
+++ main_mysql.py
@@ -10,11 +10,36 @@
 # 全部专业专硕
 import os
 
-from other.api import professional_degree_category
+from other.api import professional_degree_category, academic_master_category, academic_master_category_second
 
-result = professional_degree_category()
-for index,yjxk in enumerate(result):
-    commod ='python -m yzwspider  -ssdm 0 -yjxk '+yjxk['dm']+ " mysql -u root -p 123456 -db yzw -table t_specialty_"+str(index)
-    os.system(commod)
+
+def all_professional_degree():
+    """
+    专业硕士全部内容写入数据库
+    :return:
+    """
+    result = professional_degree_category()
+    for index,yjxk in enumerate(result):
+        commod ='python -m yzwspider  -ssdm 0 -yjxk '+yjxk['dm']+ " mysql -u root -p 123456 -db yzw -table t_specialty_"+str(index)
+        os.system(commod)
+
+
+def all_academic_master():
+    """
+    全部专业硕士信息
+    :return:
+    """
+    amc = academic_master_category()
+    for index,value in enumerate(amc):
+        # print(value['dm'])
+        amcs = academic_master_category_second(value['dm'])
+        for index_1,value_1 in enumerate(amcs):
+            commod = 'python -m yzwspider  -ssdm 0 -yjxk ' + value_1[
+                'dm'] + " mysql -u root -p 123456 -db yzw -table t_academic_" + str(index)
+            os.system(commod)
+            pass
+    pass
+
 if __name__ == '__main__':
+    all_academic_master()
     pass
